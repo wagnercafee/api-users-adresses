a36f9e23962b63523b4660ebd06e8d3b68ed8c19
wagnercafee <wagnercafee@gmail.com>
2025-12-11 09:27:01 -0300

    Cria script para rodar projeto, e construção do readme

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# setup.sh - Script de inicialização
+
+cp .env.example .env
+
+php artisan key:generate
+
+touch database/database.sqlite
+
+composer install
+
+php artisan migrate
+
+php artisan db:seed
+
+php artisan serve --host=0.0.0.0 --port=9000
